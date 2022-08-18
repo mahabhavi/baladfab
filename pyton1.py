42af15c4d9c10213cf145fee628a8236b53a5ef5
Mahaboob <dynamicduster@gmail.com>
2022-08-18 18:31:53 +0530

    Subhanallah

--- pyton1.py
+++ pyton1.py
@@ -2,6 +2,7 @@ x=int(input("entering first then"))
 if x==1:
     print("Assalamualaikum")
     print("Alhamdulillah")
+    print("Subhanallah")
 else:
     print("Walaikumassala")
     print("MashaAllah")
